ee557d9250cb6dbcc5b2973013051fee865919ae
KennyEvenCode <campb194@csusm.edu>
2023-04-14 02:27:37 -0700

    added main menu php component file

---
+++ Components/mainMenu.php
@@ -0,0 +1,21 @@
+<nav id="mainMenuOptions">
+  <input type="checkbox" />
+  <div class="bar bar1"></div>
+  <div class="bar bar2"></div>
+  <div class="bar bar3"></div>
+  <ul class="menuList">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li>
+      <a
+        href="https://github.com/KennyEvenCode/GridLock-Offline"
+        target="_blank"
+      >
+        Github
+      </a>
+    </li>
+    <li><a href="Docs/License/LICENSE" target="_blank">Licensing</a></li>
+    <li><a href="#">User Manual</a></li>
+    <li><a href="#">Developer Manual</a></li>
+  </ul>
+</nav>
